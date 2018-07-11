b6b194ad6196317d76970fcc6f5d33bac35c471c
dep78 <dep78@cornell.edu>
2018-07-11 10:42:40 -0500

    Updated setup and added requirements

--- setup.py
+++ setup.py
@@ -5,8 +5,15 @@ setup(
     version='0.0.1',
     packages=['dlhub_client'],
     description='DLHub Python client',
-    long_description=(""),
-    install_requires=[],
+    long_description=("DLHub Client is the python package for DLHub."
+                     " DLHub Client allows users to easily access"
+                     " machine learning and deep learning models"
+                     " to facilitate scientific discovery."),
+    install_requires=[
+        "pandas",
+        "requests>=2.18.4",
+        "ipywidgets"
+    ],
     python_requires=">=3.4",
     classifiers=[
         "Development Status :: 3 - Alpha",
@@ -21,6 +28,7 @@ setup(
         "DLHub",
         "Data and Learning Hub for Science",
         "machine learning",
+        "deep learning",
         "data publication",
         "reproducibility",
     ],
