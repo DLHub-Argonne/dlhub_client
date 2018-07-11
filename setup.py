5d241c027d0810f0176859ed9ac6a3748fcf7545
Ben Blaiszik <blaiszik@uchicago.edu>
2018-07-11 10:26:38 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+setup(
+    name='dlhub_client',
+    version='0.0.1',
+    packages=['dlhub_client'],
+    description='DLHub Python client',
+    long_description=(""),
+    install_requires=[],
+    python_requires=">=3.4",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering"
+    ],
+    keywords=[
+        "DLHub",
+        "Data and Learning Hub for Science",
+        "machine learning",
+        "data publication",
+        "reproducibility",
+    ],
+    license="Apache License, Version 2.0",
+    url="https://github.com/DLHub-Argonne/dlhub_client"
+)
